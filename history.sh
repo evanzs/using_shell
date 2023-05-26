8f0e3cc6587046da49d6b52172fc00bb652e8e58
evanzs <evandrofidk@hotmail.com>
2023-05-26 18:58:03 -0300

    feat(initial)::sparkles: using history cmd

---
+++ history.sh
@@ -0,0 +1,13 @@
+#CMD HISTORY - lista todos os comandos usados anteriomente.
+history
+
+clear
+# filtra os ultimos 8 comandos
+history 8
+
+#CMD !N  - executa o comando utulizado na posição N do history
+
+#CMD !!  - executa o comando alterior executado
+#CMD !?string? - busca e executa um comando parecido com a string digitada
+
+!?cl? # provavelmente irá executar o clear; 

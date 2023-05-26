b6b7aa5a68a67a4342f9f35862144e7c1bb014ea
evanzs <evandrofidk@hotmail.com>
2023-05-26 18:54:25 -0300

    feat(alias)::sparkles: using alias to shell

---
+++ alias.sh
@@ -0,0 +1,12 @@
+# serve para criar atalhos ou executar um comando grande usando uma palavra pequena
+#ex: 
+
+#cria um atalho para o comando clear, usando apenas o c
+ alias c='clear'
+
+ # CMD: ALIAS - LISTA TODOS OS COMANDOS JA CRIADOS
+ alias
+
+ # vale ressaltar que eesses ALIAS são temporarios. 
+ # para criar um permanente, é no .bashrc
+ 
